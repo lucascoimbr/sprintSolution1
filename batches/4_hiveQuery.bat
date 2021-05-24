a38777277978c554f9ce804f4cc822f46a52cedd
Logon Aluno <logonaluno@local.com>
2021-05-23 23:48:08 -0300

    todas as views finalizadas

--- batches/4_hiveQuery.bat
+++ batches/4_hiveQuery.bat
@@ -1,14 +1,12 @@
 @ECHO OFF 
 
-::Apaga os bancos preexistentes
-
 docker exec -it hive-server bash -c "hive -f /opt/hive/1_bronze.hql"
 
-::docker exec -it hive-server bash -c "hive -f /opt/hive/2_silver.hql"
+docker exec -it hive-server bash -c "hive -f /opt/hive/2_silver.hql"
 
-::docker exec -it hive-server bash -c "hive -f /opt/hive/3_gold.hql"
+docker exec -it hive-server bash -c "hive -f /opt/hive/3_gold.hql"
 
-::docker exec -it hive-server bash -c "hive -f /opt/hive/4_views.hql"
+docker exec -it hive-server bash -c "hive -f /opt/hive/4_views.hql"
 
 
 
